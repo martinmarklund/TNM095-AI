7316f485454882cf7913186920dc6cf17180ba93
Martin Marklund <martin.marklund@hotmail.se>
2019-10-01 11:19:49 +0200

    Updated behavioural tree

--- Assets/Scripts/Worker.cs
+++ Assets/Scripts/Worker.cs
@@ -2,17 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using Panda;
 
 public class Worker : MonoBehaviour
 {
-	public Transform coffee;
-	public Transform workstation;
-	public NavMeshAgent agent;
-	Vector3 agentDestination;
-	public float energy = 5.0f;
 
-	// Start is called before the first frame update
-	void Start()
+    
+    // Position related variables
+    private Vector3 agentDestination;
+    public Transform coffeeMachine;
+	public Transform workstation;
+
+    // Agent related variables
+    public NavMeshAgent agent;
+    public float energy = 5.0f;
+    public Task move;
+
+    //**** TASKS ****//
+    // Check if energy level is too low
+    [Task]
+    public bool isWorking;
+
+    [Task]
+    public bool NeedsEnergy()
+    {
+        if (energy < 0)
+        {
+            return true;
+            Task.current.Succeed();
+        }
+        else
+            return false;
+    }
+    
+    // Check if agent has arrived at current goal
+    [Task]
+    public bool arrived;
+    
+    // Behaviour tree calls this function to decide destination
+    [Task]
+    void Move(string goal)
+    {
+        if (goal == "Coffee")
+        {
+            Move(coffeeMachine);
+            isWorking = false;
+        }
+        else if (goal == "Workstation")
+        {
+            Move(workstation);
+            isWorking = true;
+        }
+
+
+        move = Task.current;
+    }
+
+    // Refill energy depending on type
+    [Task]
+    void RefillEnergy(string type)
+    {
+        if (type == "Coffee")
+        {
+            energy = 15.0f;
+        }
+
+        arrived = false;
+        Task.current.Succeed();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
 	{
 		agent = GetComponent<NavMeshAgent>();
 		Move(workstation);
@@ -20,50 +81,57 @@ public class Worker : MonoBehaviour
 
 	// Update is called once per frame
 	void Update()
-	{
+	{   
+
+        // Drain/Increase needs
 		energy -= 0.3f * Time.deltaTime;
-		updateNeeds();
+
+        IsAtGoal(agent.destination);
 
 	}
 
-	public bool isAtGoal(Transform goal)
+    // True if agent is close enough to goal, otherwise false
+	public void IsAtGoal(Vector3 goal)
 	{
-		if (Vector3.Distance(agent.transform.position, goal.position) < 1.0f)
+		if (Vector3.Distance(agent.transform.position, goal) < 2.0f)
 		{
-			return true;
-		}
-		return false;
+            arrived = true;
+            move.Complete(true);
+        }
+        else
+            arrived = false;
 	}
 
-    
+    // Overloaded move that is used within script to assign destination
+    void Move(Transform goal)
+    {
+        arrived = false;
+        agent.destination = goal.position;
+    }
 
-    void updateNeeds()
-	    {
-		    if (energy < 5)
-		    {
-			    Move(workstation);
+    /* 
+    void CheckNeeds()
+         {
+             if (energy < 5)
+             {
+                 Move(workstation);
+             }
 
-		    }
+             if (energy < 0)
+             {
+                 Move(coffee);
 
-		    if (energy < 0)
-		    {
-			    Move(coffee);
+                 if (isAtGoal(coffee))
+                 {
+                     energy = 5.0f;
+                 }
+             }
+         }
 
-			    if (isAtGoal(coffee))
-			    {
-				    energy = 5.0f;
-			    }
-		    }
-	    }
+     void NeedCoffee() {
 
-	void needCoffee() {
-
-	}
-	void Move(Transform goal)
-	{
-		agent.destination = goal.position;
-	}
-
+     }
+     */
 }
 
 
