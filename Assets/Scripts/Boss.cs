6f130518f309ed3d126a3140f8f64477f2f38eed
Erik Akesson <mail@eakesson.se>
2019-10-03 17:12:34 +0200

    Boss Ai patrol created and done

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using System.Linq;
+using Panda;
+
+public class Boss : MonoBehaviour
+{
+
+    public Transform office;
+    public Vector3[] patrolPoints;
+    public NavMeshAgent agent;
+    public bool monitorWorkers;
+
+    private bool atOffice;
+    private float keyStrokeTimer;
+    private Task move;
+    public int nextPP;
+    public bool arrived;
+    public bool goCrazy;
+
+    [Task]
+    public bool isNextPP;
+
+    [Task]
+    public bool playerInControl()    
+    {
+        if (keyStrokeTimer < 0)
+        {
+            return true;            
+        }
+        else
+            return false;
+    }    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Find and setting up the patrolpoints
+        GameObject[] Gm = GameObject.FindGameObjectsWithTag("PatrolPoint").OrderBy(go => go.name).ToArray();
+        patrolPoints = new Vector3[Gm.Length];
+        for (int i = 0; i < Gm.Length; i++ )
+        {            
+            patrolPoints[i] = Gm[i].transform.position;
+        }
+
+        keyStrokeTimer = 0;
+        isNextPP = false;
+        nextPP = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {        
+        IsAtGoal();                            
+    }   
+
+    // True if agent is close enough to goal, otherwise false
+    public void IsAtGoal()
+    {
+        Debug.Log(agent.radius);
+        if (!arrived && Vector3.Distance(gameObject.transform.position, agent.destination) < 2.0f)
+        {                     
+            move.Complete(true);
+            arrived = true;
+            nextPP++;
+        }
+    }
+
+    [Task]
+    void GoToOffice()
+    {
+        arrived = false;
+        agent.destination = office.position;
+        move = Task.current;
+        isNextPP = true;
+    }
+
+    [Task]
+    public void goToPP()
+    {
+        //Debug.Log(nextPP + " : " + patrolPoints.Length);
+        if(nextPP > patrolPoints.Length-1)
+        {
+            isNextPP = false;
+            Task.current.Fail();
+            nextPP = -1;
+        }
+        else
+        {            
+            //Debug.Log("hit " + nextPP);
+            //agent.destination = new Vector3(0,0,0);
+            agent.destination = patrolPoints[nextPP];            
+            move = Task.current;            
+            arrived = false;
+        }        
+    }
+}
---
+++ Assets/Scripts/Worker_Erik.cs
@@ -0,0 +1,186 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using Panda;
+
+public class Worker_Erik : MonoBehaviour
+{
+
+    
+    // Position related variables
+    private Vector3 agentDestination;
+    public Transform coffeeMachine;
+	public Transform workstation;
+    public Transform boss;
+
+    // Agent related variables
+    public NavMeshAgent agent;
+    public float energy = 5.0f;
+    public float occupation = 1.0f;
+    public Task move;
+
+    //**** TASKS ****//
+    // Check if energy level is too low
+    [Task]
+    public bool isWorking;
+    [Task]
+    public bool isBossNear;
+
+    [Task]
+    public bool NeedsEnergy()
+    {
+        if (energy < 0)
+        {
+            return true;
+            Task.current.Succeed();
+        }
+        else
+            return false;
+    }
+    
+    // Check if agent has arrived at current goal
+    [Task]
+    public bool arrived;
+    
+    // Behaviour tree calls this function to decide destination
+    [Task]
+    void Move(string goal)
+    {
+        if (goal == "Coffee")
+        {
+            Move(coffeeMachine);
+            isWorking = false;
+        }
+        else if (goal == "Workstation")
+        {
+            Move(workstation);
+            isWorking = true;
+        }
+
+
+        move = Task.current;
+    }
+
+    // Refill energy depending on type
+    [Task]
+    void RefillEnergy(string type)
+    {
+        if (type == "Coffee")
+        {
+            energy = 5.0f;
+        }
+
+        arrived = false;
+        Task.current.Succeed();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+		Move(workstation);
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+        // Check what the agent is doing
+        occupation = Occupation();
+
+        // Drain/Increase needs
+        energy -= Time.deltaTime * occupation * 0.3f;
+
+        IsAtGoal(agent.destination);
+        IsBossNear();
+	}
+
+    // True if agent is close enough to goal, otherwise false
+	public void IsAtGoal(Vector3 goal)
+	{
+		if (Vector3.Distance(agent.transform.position, goal) < 2.0f)
+		{
+            arrived = true;
+            move.Complete(true);
+        }
+        else
+            arrived = false;
+	}
+
+    public void IsBossNear()
+    {
+        if (Vector3.Distance(gameObject.transform.position, boss.position) < 20.0f) //TODO: hardcoded distance, should only check 2d
+        {
+            isBossNear = true;
+            //If no wall is between            
+            RaycastHit[] hits;
+            hits = Physics.RaycastAll(gameObject.transform.position, (boss.position - gameObject.transform.position), 20.0f); //TODO have same distance as above
+
+            Debug.DrawRay(gameObject.transform.position, Vector3.Normalize(boss.position - gameObject.transform.position) * 20.0f, Color.red);
+
+            //Debug.Log(hits[hits.Length - 1].distance);
+
+            for (int i = 0; i < hits.Length; i++)
+            {
+                Debug.Log(hits[i].transform.tag);
+                if(hits[i].transform.tag == "Boss")
+                {
+                    break;
+                }
+                else if (hits[i].transform.tag == "Wall") //TODO if several floors check for roof hit too
+                {
+                    isBossNear = false;
+                    break;
+                }                            
+            }              
+        }
+        else
+        {
+            isBossNear = false;
+        }
+    }
+
+    // Overloaded move that is used within script to assign destination
+    void Move(Transform goal)
+    {
+        arrived = false;
+        agent.destination = goal.position;
+    }
+
+    public float Occupation()
+    {
+        if (isWorking)
+            return 1.3f;
+        else if (!arrived)
+            return 0.5f;
+        else
+            return 1.0f;
+    }
+
+    /* 
+    void CheckNeeds()
+         {
+             if (energy < 5)
+             {
+                 Move(workstation);
+             }
+
+             if (energy < 0)
+             {
+                 Move(coffee);
+
+                 if (isAtGoal(coffee))
+                 {
+                     energy = 5.0f;
+                 }
+             }
+         }
+
+     void NeedCoffee() {
+
+     }
+     */
+}
+
+
