503f281b5010272a1b0b5db754d5798a95e56346
EAkesson <mail@eakesson.se>
2019-10-07 10:34:05 +0200

    Cleaned upp boss code, did comments

--- Assets/Scripts/Boss.cs
+++ Assets/Scripts/Boss.cs
@@ -6,27 +6,28 @@ using System.Linq;
 using Panda;
 
 public class Boss : MonoBehaviour
-{   
+{
 
+    // Position related variables
     public Transform office;
-    public Vector3[] patrolPoints;
-    public NavMeshAgent agent;
-    public bool monitorWorkers;
+    public Vector3[] patrolPoints;  
 
-    private bool atOffice;
-    public float keyStrokeTimer;
-    private Task move;
-    public int nextPP;
-    public bool arrived;
-    public bool goCrazy;
+    //Agent Related variables
+    public NavMeshAgent agent;
+    
+    private float keyStrokeTimer;
     private float speed;
+    private Task move;
+    private int nextPP;
+    private bool arrived;        
     private bool playerWasInControll;
 
     [Task]
-    public bool isNextPP;
+    public bool isNextPP; //If we have a next patrolpoint
 
     [Task]
-    public bool playerInControl()
+    //If the player controls the boss
+    public bool playerInControl() 
     {
         if (keyStrokeTimer > 0)
         {
@@ -47,6 +48,7 @@ public class Boss : MonoBehaviour
             patrolPoints[i] = Gm[i].transform.position;
         }
 
+        //Init variables
         keyStrokeTimer = 0;
         isNextPP = false;
         nextPP = -1;
@@ -59,7 +61,7 @@ public class Boss : MonoBehaviour
     void Update()
     {        
         IsAtGoal();
-        Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+        
 
         keyStrokeTimer -= Time.deltaTime;
         if (Input.anyKey)
@@ -69,14 +71,14 @@ public class Boss : MonoBehaviour
             playerWasInControll = true;
             isNextPP = true;
             readKeyEv();
-        }        
+            //Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));        Can use this instead?
+        }
     }          
 
     // True if agent is close enough to goal, otherwise false
     public void IsAtGoal()
-    {
-        //Debug.Log(agent.radius);
-        if (!arrived && agent.remainingDistance < 0.2f)
+    {        
+        if (!arrived && agent.remainingDistance < 0.1f)
         {                     
             move.Complete(true);
             arrived = true;
@@ -105,6 +107,7 @@ public class Boss : MonoBehaviour
     }
 
     [Task]
+    //Function to set Office as destination (called from tree)
     public void GoToOffice()
     {
         arrived = false;
@@ -113,30 +116,34 @@ public class Boss : MonoBehaviour
         isNextPP = true;
     }
     
+    //Fucntion to find the closest patrolpoint to the boss
     public void FindClosestPP()
     {
         playerWasInControll = false;        
         NavMeshPath path = new NavMeshPath();
         float minPathLength = 99999.0f;
 
+        //Loop trough all pp
         for (int i = 0; i < patrolPoints.Length+1; i++)
         {
             path.ClearCorners();
             float pathL = 0.0f;
 
+            //Calculate the path
             if(i < patrolPoints.Length)
                 agent.CalculatePath(patrolPoints[i], path);            
             else
                 agent.CalculatePath(office.position, path);
 
+            //If path is valid
             if (path.status == NavMeshPathStatus.PathComplete)
             {
-                
+                //Calc the leght of the path            
                 for (int j = 1; j < path.corners.Length; ++j)
                 {
                     pathL += Vector3.Distance(path.corners[j - 1], path.corners[j]);
                 }
-                
+                //If this path is shorter than the current min set it as the current destiantion
                 if (pathL < minPathLength)
                 {
                     minPathLength = pathL;
@@ -147,14 +154,17 @@ public class Boss : MonoBehaviour
     }
 
     [Task]
+    //Function to set a patrolpoint as destination (called from tree)
     public void GoToPP()
     {
+        //If we have been in controll we need to find closest patrol point
         if (playerWasInControll)
         {
             FindClosestPP();
         }
-        
-        //Debug.Log(nextPP + " : " + patrolPoints.Length);
+
+        //If we are at the last patrolpoint we set an end to the patroling and let the tree know by isNextPP
+        //Else we set next pp as our destiantion
         if (nextPP > patrolPoints.Length-1)
         {
             isNextPP = false;
@@ -162,9 +172,7 @@ public class Boss : MonoBehaviour
             nextPP = -1;
         }
         else
-        {            
-            //Debug.Log("hit " + nextPP);
-            //agent.destination = new Vector3(0,0,0);
+        {                        
             agent.destination = patrolPoints[nextPP];            
             move = Task.current;            
             arrived = false;
